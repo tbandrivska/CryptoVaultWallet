2ed4bdd96c311ac1982a14b73615080233cd7a5e
Nelumi8 <70643925+Nelumi8@users.noreply.github.com>
2026-04-08 15:09:53 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,12 @@
-import app from "./app.js";
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
 import express from "express";
 import transactionRoutes from "./routes/transactionRoutes.js";
 
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 app.use("/api/transactions", transactionRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
